c5895acecd8b45d4a0aaa549bb80b681c69ef455
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-25 11:51:17 +0100

    Created add functionality for yargs

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const yargs = require("yargs")
+
+const { Movie, movieArr } = require("./utils")
+
+//console.log(process.argv)
+//console.log(yargs.argv)
+
+const app = (yargsObj) => {
+    if (yargsObj.add) {
+        const movie = new Movie(yargsObj.title, yargsObj.actor)
+        movie.add()
+        console.log(movieArr)
+    } else {
+        console.log("Incorrect Command")
+    }
+}
+
+app(yargs.argv)
